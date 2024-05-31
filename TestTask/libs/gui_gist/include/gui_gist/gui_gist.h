932091fb79b2548149907b519380f484a3606426
Polina <prrshka@mail.ru>
2024-05-31 14:05:17 +0300

    попытка в нову иблиотеку подключить созданную

--- TestTask/libs/gui_gist/include/gui_gist/gui_gist.h
+++ TestTask/libs/gui_gist/include/gui_gist/gui_gist.h
@@ -2,6 +2,13 @@
 #define GUI_GIST_H
 
 #include <iostream>
+#include <QtCharts>
+#include "qeneri_rand_num.h"
 
+class GistProcessor {
+public:
+    GistProcessor(RandomNumberGenerator);
+
+};
 
 #endif
